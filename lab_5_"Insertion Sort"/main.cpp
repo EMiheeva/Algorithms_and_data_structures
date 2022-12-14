9f57787f79e2a212c6f64c5f244a827b47382742
EMiheeva <90220808+EMiheeva@users.noreply.github.com>
2022-12-14 14:17:40 +0200

    Create main.cpp

---
+++ lab_5_"Insertion Sort"/main.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+const int N = 10;
+
+
+void InsertionSort(int mas[], int n)
+{
+    int i, j, key;
+    for (i = 1; i < n; i++)
+    {
+        key = mas[i];
+        j = i - 1;
+ 
+        // Перемещаем элементы mas[0..i-1], 
+        // которые больше ключа, на одну
+        // позицию вперед по сравнению с их
+        // текущей позиции
+        while (j >= 0 && mas[j] > key)
+        {
+            mas[j + 1] = mas[j];
+            j = j - 1;
+        }
+        mas[j + 1] = key;
+    }
+}
+
+
+int main()
+{
+	auto mass = new int[N];
+	
+    cout << "Введите 10 элементов типа int вашего массив: " <<endl;
+    
+    for (int k = 0; k < N; k++) {
+		cin >> mass[k];
+	}
+	cout << "Ваш массив: ";
+	for (int k = 0; k < N; k++) {
+		cout << mass[k] << " ";
+	}
+    cout << "" <<endl;
+
+	InsertionSort(mass, N);
+	cout << "Отсортированный массив: ";
+	for (int k = 0; k < N; k++) {
+		cout << mass[k] << " ";
+	}
+	return 0;
+}
